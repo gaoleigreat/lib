3d0a3145032b6a96037acb4e9216b3a436e752f8
xiaodao <513684652@qq.com>
2019-08-08 20:40:45 +0800

    Excel 模板

---
+++ lib-excel/src/main/java/com/lego/survey/lib/excel/element/EObject.java
@@ -0,0 +1,29 @@
+package com.lego.survey.lib.excel.element;
+
+/**
+ * @author xiaodao
+ */
+public abstract class EObject {
+
+    public abstract Object getValByKey(String key);
+
+    public boolean isPic(String key) {
+        return this.getValByKey(key) instanceof EPic;
+    }
+
+    public String getTextByKey(String key) {
+        if (!isPic(key)) {
+            return (String) this.getValByKey(key);
+        } else {
+            return null;
+        }
+    }
+
+    public EPic getPicByKey(String key) {
+        if (isPic(key)) {
+            return (EPic) this.getValByKey(key);
+        } else {
+            return null;
+        }
+    }
+}
