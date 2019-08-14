1466edcafc7f9efa16471e212d67459f6e648f67
yanglf <899117>
2019-08-14 12:56:27 +0800

    1

--- lib-common/src/main/java/com/survey/lib/common/consts/RespConsts.java
+++ lib-common/src/main/java/com/survey/lib/common/consts/RespConsts.java
@@ -2,59 +2,59 @@ package com.survey.lib.common.consts;
 
 /**
  * @author yanglf
- * @description   响应信息定义
+ * @description 响应信息定义
  * @since 2018/12/22
  **/
 public class RespConsts {
 
 
-    public static final String SUCCESS = "success";
-    public static final String FAILURE = "fail";
+    public static final String SUCCESS = "操作成功";
+    public static final String FAILURE = "操作失败";
 
 
-    public  static  final int SUCCESS_RESULT_CODE=1;
-    public static  final  String SUCCESS_RESULT_MSG="请求成功";
+    public static final int SUCCESS_RESULT_CODE = 1;
+    public static final String SUCCESS_RESULT_MSG = "请求成功";
     /**
      * 权限认证失败
      */
-    public  static  final  int FAIL_NOPRESSION_CODE=-1;
-    public  static  final String FAIL_NOPRESSION_MSG="权限校验失败，请联系管理员";
+    public static final int FAIL_NOPRESSION_CODE = -1;
+    public static final String FAIL_NOPRESSION_MSG = "权限校验失败，请联系管理员";
 
     /**
      * 登录失败  msg 为详细错误原因
      */
-    public  static  final int FAIL_LOGIN_CODE=-2;
+    public static final int FAIL_LOGIN_CODE = -2;
 
     /**
      * 服务内部错误
      */
-    public static  final int ERROR_SERVER_CODE=-3;
+    public static final int ERROR_SERVER_CODE = -3;
     /**
      * 调用超时
      */
-    public static final int ERROR_CALLTIMEOUT_CODE=-4;
+    public static final int ERROR_CALLTIMEOUT_CODE = -4;
 
     /**
      * 其他错误
      */
-    public static final int ERROR_OTHER_CODE=-5;
+    public static final int ERROR_OTHER_CODE = -5;
 
     /**
      * 结果错误
      */
-    public static final int FAIL_RESULT_CODE=-6;
+    public static final int FAIL_RESULT_CODE = -6;
 
     /**
-     *  数据异常
+     * 数据异常
      */
-    public  static  final int DATA_ERROR=-7;
+    public static final int DATA_ERROR = -7;
 
     /**
      * 失败
      */
     public interface Failure {
         String ret = FAILURE;
-        int retCode=FAIL_RESULT_CODE;
+        int retCode = FAIL_RESULT_CODE;
         String msg = "请求失败";
     }
 
@@ -63,20 +63,20 @@ public class RespConsts {
      */
     public interface Success {
         String ret = SUCCESS;
-        int retCode=SUCCESS_RESULT_CODE;
+        int retCode = SUCCESS_RESULT_CODE;
         String msg = "请求成功";
     }
 
 
-    public interface  DataErrorType{
+    public interface DataErrorType {
         /**
-         *   id 重复  异常
+         * id 重复  异常
          */
-        int REPEAT_ID_ERROR=1;
+        int REPEAT_ID_ERROR = 1;
         /**
          * 数据类型异常
          */
-        int DATA_TYPE_ERROR=2;
+        int DATA_TYPE_ERROR = 2;
     }
 
 }
